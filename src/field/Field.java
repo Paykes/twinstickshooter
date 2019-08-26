646f62a9bea3fc8320edf56e85e512745d030f1b
Paykes <jameszzsmith01@gmail.com>
2019-08-26 19:25:20 +1000

    Trial at Field (not working yet)

---
+++ src/field/Field.java
@@ -0,0 +1,15 @@
+package field;
+
+import geometry.Position;
+import thing.Thing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Field {
+    private ArrayList<Thing> Field = new ArrayList<Thing>();
+
+    public Field addThing(Thing, Position) {
+        Field.add(Thing);
+    }
+}
